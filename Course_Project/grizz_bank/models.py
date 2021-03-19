5bdef5e50556eb6b01ef56c14e59af6fa65f40be
Mike Moore <sisyphus168@gmail.com>
2021-03-19 15:46:38 -0400

    Finished initial version of model classes, tweaked settings to refer to grizzbank, migrations

--- Course_Project/grizz_bank/models.py
+++ Course_Project/grizz_bank/models.py
@@ -15,31 +15,30 @@ class Client(models.Model):
 
 
 class Username(models.Model):
-    username = models.fields.CharField(max_length=45, primary_key=True)
+    username = models.fields.CharField(max_length=45)
     client = models.ForeignKey(Client, on_delete=models.CASCADE, primary_key=True)
-    client_id = models.fields.IntegerField(primary_key=True)
 
 
 class PhoneNumber(models.Model):
     client = models.ForeignKey(Client, on_delete=models.CASCADE, primary_key=True)
-    phone_number = models.fields.CharField(max_length=10, primary_key=True)
+    phone_number = models.fields.CharField(max_length=10)
 
 
 class InterestRate(models.Model):
     acct_type = models.fields.CharField(max_length=1, primary_key=True)
-    interest_rate = models.fields.DecimalField(default=0.0)
+    interest_rate = models.fields.DecimalField(max_digits=3, decimal_places=2, default=0.0)
 
 
 class Email(models.Model):
     client = models.ForeignKey(Client, on_delete=models.CASCADE, primary_key=True)
-    email = models.fields.CharField(max_length=50, primary_key=True)
+    email = models.fields.CharField(max_length=50)
 
 
 class Account(models.Model):
     acct_id = models.fields.AutoField(primary_key=True)
-    acct_bal = models.fields.DecimalField(max_digits=5, decimal_places=2, null=False)
+    acct_bal = models.fields.DecimalField(max_digits=8, decimal_places=2, null=False)
     acct_type = models.fields.CharField(max_length=1)
-    client = models.ForeignKey(Client, on_delete=models.CASCADE, primary_key=True)
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
 
 
 class RequestReset(models.Model):
