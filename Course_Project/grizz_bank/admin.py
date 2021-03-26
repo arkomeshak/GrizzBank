c677c357b3111c25530b280a6064f7b31b8a6d71
Mike Moore <sisyphus168@gmail.com>
2021-03-26 13:38:48 -0400

    Grizz Bank admin.py file now has Grizz Bank model classes associated

--- Course_Project/grizz_bank/admin.py
+++ Course_Project/grizz_bank/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import *
+
 
 # Register your models here.
+admin.site.register(Client)
+admin.site.register(Username)
+admin.site.register(Email)
+admin.site.register(PhoneNumber)
+admin.site.register(Account)
+admin.site.register(InterestRate)
+admin.site.register(RequestReset)
+
