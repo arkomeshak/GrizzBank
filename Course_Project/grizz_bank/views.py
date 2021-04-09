3ec1a98c19f19cee65a0d154b27e3b987021c819
Mike Moore <sisyphus168@gmail.com>
2021-04-09 16:33:29 -0400

    Transfer handler view implemented, transfer template updated to work with handler.

--- Course_Project/grizz_bank/views.py
+++ Course_Project/grizz_bank/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from .models import *  # import all of the model classes
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.db import transaction, IntegrityError
+
+import re  # regular expressions
+import decimal
 
 # Create your views here.
 
@@ -63,7 +67,7 @@ def transfer(request):
         """
         # dictionary mapping account type key to an actual account type name
         types = {"S": "Savings", "C": "Checking"}
-        # List comprehension returning a list of dicitonaries containing account type, balance, and ID
+        # List comprehension returning a list of dictionaries containing account type, balance, and ID
         acct_data = [{"type": types[acct.acct_type], "bal": acct.acct_bal, "id": acct.acct_id} for acct in accounts]
         return acct_data
 
@@ -74,6 +78,7 @@ def transfer(request):
         client_accounts = Account.objects.filter(client_id=client_id)
         # List comprehension to build a list of python dictionaries containing account data into the context
         context["account_data"] = create_account_data_list(client_accounts)
+        context["uname"] = uname
         context["error"] = False
     except Exception as e:
         # If an error happens log to console, set context error flag true, and make account data an empty list
@@ -102,8 +107,86 @@ def login_handler(request):
     pass
 
 
+@transaction.atomic
 def transfer_handler(request):
-    pass
+    """
+    Handles incoming POST request sent from the transfer page requesting funds
+    :param request: POST request with account ids to transfer to, and from, as well as quantities
+    :return: HTTP response, or redirect
+    """
+    def get_account_ids(post_dict):
+        """
+        Helper function extracting the acount ids from the POST request's keys
+        :param post_dict: python dictionary
+        :return: (int: from_id, int: to_id)
+        """
+        from_id, to_id = None, None
+        for key in post_dict.keys():
+            from_match = re.match("from_acct\d*", key)
+            to_match = re.match("to_acct\d*", key)
+            # matches can have the id extracted by splitting string on "acct" substring and selecting second half
+            if from_match is not None:
+                from_id = int(from_match.string.split("acct")[1])
+                continue
+            if to_match is not None:
+                to_id = int(to_match.string.split("acct")[1])
+        if (from_id is None) or (to_id is None):
+            raise RuntimeError("transfer handler error: no valid to/from account IDs found")
+        return from_id, to_id
+
+    uname = ""  # initially set uname to empty string
+    try:
+        # Check that expected the request was via POST, and correct fields are in the request
+        if request.method != "POST":
+            raise RuntimeError("Transfer handler expects a post")
+        post = request.POST
+        for field in ["transfer_amount", "uname"]:
+            if field not in post:
+                raise RuntimeError(f"Transfer Handler expected the missing field {field} to be in the request.")
+        # Retrieve account data from db using data sent by post
+        amount, uname = post["transfer_amount"], post["uname"]
+        if not (len(amount) > 0 or amount.isnumeric()):
+            raise RuntimeError(f"Transfer handler error: Invalid amount passed by user. amount={amount}")
+        # Convert ammount to a decimal type from the string
+        amount = decimal.Decimal(float(amount))
+        from_id, to_id = get_account_ids(post)
+        print(f"amount: {amount}")
+        if amount <= 0.0:
+            raise RuntimeError(f"transfer handler error: invalid transfer amount. Cannot transfer amount <= 0. amount {amount}")
+        from_acct = Account.objects.get(pk=from_id)
+        # Check that the account transfering from has enough funds
+        from_bal = from_acct.acct_bal
+        if from_bal < amount:
+            errmsg = f"Transfer Handler error: acct# {from_id} has insufficient funds: {from_acct.acct_bal:.2f} needed {amount:.2f}"
+            raise RuntimeError(errmsg)
+        to_acct = Account.objects.get(pk=to_id)
+        # Check that accounts belong to same user
+        acct_id = Client.objects.get(username=uname).client_id
+        if to_acct.client.pk != from_acct.client.pk or to_acct.client.pk != acct_id or from_acct.client.pk != acct_id:
+            print(f"acct_id: {acct_id} from_client_id: {from_acct.client.pk} to_client_id: {to_acct.client.pk}")
+            raise RuntimeError(f"Transfer Handler error: account owner mismatch: from owner ID: {from_acct.client} to owner id:{to_acct.client}")
+        # Perform the transfer as an ACID transaction
+        with transaction.atomic():
+            from_acct.acct_bal -= amount
+            to_acct.acct_bal += amount
+            from_acct.save()
+            to_acct.save()
+        # transfer successfully occurred
+        return HttpResponseRedirect(f"/grizz_bank?uname={uname}?status=transfer_success")
+
+    except IntegrityError as e:
+        print(e)
+        print(f"POST data: {post}")
+        return HttpResponseRedirect(f"/grizz_bank/transfer/?uname={uname}&error_msg=transfer_transaction_error")
+    except RuntimeError as e:
+        print(e)
+        print(f"POST data: {post}")
+        return HttpResponseRedirect(f"/grizz_bank/transfer/?uname={uname}&error_msg=transfer_invalid_transfer_error")
+    except Exception as e:
+        print(e)
+        print(f"Post data: {post}")
+        return HttpResponseRedirect(f"/grizz_bank/transfer/?uname={uname}&error_msg=unknown_transfer_error")
+
 
 
 def deposit_handler(request):
